ae1029c5860b0264f9e3ed79aba7452521ffa4bc
Lucas Henrique Gonzaga de Sales <71048167+hipera09@users.noreply.github.com>
2021-06-22 00:19:23 -0300

    Update PessoaRepository.java
    Adição do salvamento em arquivo de pessoas.

--- src/ruraldevs/repository/PessoaRepository.java
+++ src/ruraldevs/repository/PessoaRepository.java
@@ -5,17 +5,83 @@ import java.util.ArrayList;
 import ruraldevs.beans.Pessoa;
 
 public class PessoaRepository extends GenericRepository<Pessoa> {
-	private static PessoaRepository instance;
+	
+	private static final long serialVersionUID = -170031939952062163L;
+	private static PessoaRepository instance=null;
 
 	private PessoaRepository() {
-		super();
 	}
 
 	public static PessoaRepository getInstance() {
 		if (instance == null) {
-			instance = new PessoaRepository();
+			instance = lerDoArquivo();
 		}
 		return instance;
 	}
 
 }
+
+public void addPessoa(Pessoa pessoa) {
+		this.addDado(pessoa);
+		lerDoArquivo();
+	}
+
+	public List<Pessoa> getPessoas() {
+		return this.getDados();
+	}
+
+	public void deletePessoa(Pessoa pessoa) {
+		this.deleteDado(pessoa);
+		
+	}
+
+	public void updatePessoa(Pessoa pessoa) {
+		this.updateDado(pessoa);
+		
+	}
+		private static PessoaRepository lerDoArquivo() {
+			PessoaRepository instanciaLocal = null;
+
+	    File in = new File("pessoas.dat");
+	    FileInputStream fis = null;
+	    ObjectInputStream ois = null;
+	    try {
+	      fis = new FileInputStream(in);
+	      ois = new ObjectInputStream(fis);
+	      Object o = ois.readObject();
+	      instanciaLocal = (PessoaRepository) o;
+	    } catch (Exception e) {
+	      instanciaLocal = new PessoaRepository();
+	    } finally {
+	      if (ois != null) {
+	        try {
+	          ois.close();
+	        } catch (IOException e) {
+	        }
+	      }} return instanciaLocal;
+	    }
+	
+	  public void salvarArquivo() {
+		    if (instance == null) {
+		      return;
+		    }
+		    File out = new File("pessoas.dat");
+		    FileOutputStream fos = null;
+		    ObjectOutputStream oos = null;
+
+		    try {
+		      fos = new FileOutputStream(out);
+		      oos = new ObjectOutputStream(fos);
+		      oos.writeObject(instance);
+		    } catch (Exception e) {
+		      e.printStackTrace();
+		    } finally {
+		      if (oos != null) {
+		        try {
+		          oos.close();
+		        } catch (IOException e) {
+		        }
+		      }
+		    }}
+}
+
