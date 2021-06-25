c9486649c296849a06a8c56a5ab0e85e4fc321a8
Lucas Henrique Gonzaga de Sales <71048167+hipera09@users.noreply.github.com>
2021-06-25 15:17:01 -0300

    Add files via upload

---
+++ src/ruraldevs/beans/GrupoEnum.java
@@ -0,0 +1,35 @@
+package Beans;
+
+public enum GrupoEnum{
+	
+	
+	pessoas43mais("Pesssoas com 43 anos ou mais"),
+	trabalhadores_industriais("Trabalhadores Industriais"),
+	trabalhadores_a_s("Trabalhadores da assistência social"),
+	caminhoneiros("Caminhoneiros"),
+	forcas_seg_salv("Forças de segurança e salvamento"),
+	trabalhadores_limpeza("Trabalhadores da Limpeza Urbana e Manejo de Resíduos Sólidos"),
+	pessoas_de_rua("Pessoas em Situação de Rua"),
+	transporte_coletivo_rodoviario("Trabalhadores de Transporte Coletivo Rodoviário Passageiros Urbano e de Longo Curso"),
+	transporte_metro_ferroviario("Trabalhadores de Transporte Metroviário e Ferroviário"),
+	transporte_aereo("Trabalhadores de Transporte Aéreo"),
+	portuarios("Trabalhadores Portuários"),
+	transporte_aquaviario("Trabalhadores de Transporte Aquaviário"),
+	gravidas("Grávidas e Puérperas "),
+	deficientes("Pessoas com deficiência"),
+	comorbidades("Pessoas com comorbidades"),
+	educacao("Trabalhadores da Educação"),
+	saude("Trabalhadores da Saúde");
+
+	private String nomeGrupo;
+	GrupoEnum(String nomeGrupo) {
+		this.nomeGrupo = nomeGrupo;
+
+	}
+	@Override
+    public String toString() {
+        return nomeGrupo;
+	}
+
+
+}
