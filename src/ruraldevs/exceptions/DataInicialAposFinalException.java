5cbdbf4cf12930d505f755d029296f71fbe79a04
Gian <gdass8000@gmail.com>
2021-06-25 18:41:59 -0300

    Adicionada a tela com os dados da COVID-19

---
+++ src/ruraldevs/exceptions/DataInicialAposFinalException.java
@@ -0,0 +1,32 @@
+package ruraldevs.exceptions;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class DataInicialAposFinalException extends Exception {
+    private LocalDate dataInicial;
+    private LocalDate dataFinal;
+
+    public DataInicialAposFinalException(LocalDate dataInicial, LocalDate dataFinal) {
+        super(String.format("Data inicial (%s) vem após a data final (%s).", DateTimeFormatter.ofPattern("dd/MM/YYYY").format(dataInicial), DateTimeFormatter.ofPattern("dd/MM/YYYY").format(dataFinal)));
+        this.dataInicial = dataInicial;
+        this.dataFinal = dataFinal;
+    }
+
+    public LocalDate getDataInicial() {
+        return dataInicial;
+    }
+
+    public void setDataInicial(LocalDate dataInicial) {
+        this.dataInicial = dataInicial;
+    }
+
+    public LocalDate getDataFinal() {
+        return dataFinal;
+    }
+
+    public void setDataFinal(LocalDate dataFinal) {
+        this.dataFinal = dataFinal;
+    }
+
+}
--- src/ruraldevs/gui/mainTelas.java
+++ src/ruraldevs/gui/mainTelas.java
@@ -1,4 +1,4 @@
-package gui;
+package ruraldevs.gui;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -7,67 +7,64 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
-public class mainTelas extends Application { 
-	
+public class mainTelas extends Application {
 	private static Stage stg;
 	private static Scene mainScene;
 	private static Scene cadasScene;
 	private static Scene agendaScene;
 	private static Scene aLoginScene;
-	
-		
-	    @Override
-	   public void start(Stage primaryStage) throws Exception{
-	    	
+	private static Scene dadosScene;
 
-	    	stg=primaryStage;
-	    	
-	        Parent fxmlMain=FXMLLoader.load(getClass().getResource("/gui/sample.fxml"));
-	        mainScene = new Scene(fxmlMain);
-	        
-	        Parent fxmlCadas=FXMLLoader.load(getClass().getResource("/gui/cadassample.fxml"));
-	        cadasScene = new Scene(fxmlCadas);
-	        
-	        Parent fxmlAgenda=FXMLLoader.load(getClass().getResource("/gui/agendasample.fxml"));
-	        agendaScene = new Scene(fxmlAgenda);
-	        
-	        Parent fxmlAfterLogin=FXMLLoader.load(getClass().getResource("/gui/aloginsample.fxml"));
-	        aLoginScene = new Scene(fxmlAfterLogin);
-	        
-	        primaryStage.setScene(mainScene);
-	        
-	        primaryStage.setTitle("VacinaBrasil");
-	        primaryStage.setResizable(false);
-	        primaryStage.getIcons().add(new Image("/gui/icone.png"));
-	        primaryStage.show();
-	       
-	       
-	    }
-	    
-	   public static void  changeScreen(String scr) {
-		   switch(scr) {
-		   case "main":
-			   stg.setScene(mainScene);
-			   break;
-		   case "cadas":
-			   stg.setScene(cadasScene);
-			   break;
-		   case "agenda":
-			   stg.setScene(agendaScene);
-			   break;
-		   case "alogin":
-			   stg.setScene(aLoginScene);
-			   break;
-		   }
-	   }
-	   
-	  
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		stg = primaryStage;
 
-	   
-	   public static void main(String[] args) {
-		   
-	        launch(args);
-	    }
+		Parent fxmlMain = FXMLLoader.load(getClass().getResource("/ruraldevs/gui/sample.fxml"));
+		mainScene = new Scene(fxmlMain);
+
+		Parent fxmlCadas = FXMLLoader.load(getClass().getResource("/ruraldevs/gui/cadassample.fxml"));
+		cadasScene = new Scene(fxmlCadas);
+
+		Parent fxmlAgenda = FXMLLoader.load(getClass().getResource("/ruraldevs/gui/agendasample.fxml"));
+		agendaScene = new Scene(fxmlAgenda);
+
+		Parent fxmlAfterLogin = FXMLLoader.load(getClass().getResource("/ruraldevs/gui/aloginsample.fxml"));
+		aLoginScene = new Scene(fxmlAfterLogin);
+
+		Parent fxmlTelaDados = FXMLLoader.load(getClass().getResource("/ruraldevs/gui/telaDados.fxml"));
+		dadosScene = new Scene(fxmlTelaDados);
+
+		primaryStage.setScene(mainScene);
+
+		primaryStage.setTitle("VacinaBrasil");
+		primaryStage.setResizable(false);
+		primaryStage.getIcons().add(new Image("/ruraldevs/gui/icone.png"));
+		primaryStage.show();
+	}
+
+	public static void changeScreen(String scr) {
+		switch (scr) {
+			case "main":
+				stg.setScene(mainScene);
+				break;
+			case "cadas":
+				stg.setScene(cadasScene);
+				break;
+			case "agenda":
+				stg.setScene(agendaScene);
+				break;
+			case "alogin":
+				stg.setScene(aLoginScene);
+				break;
+			case "dados":
+				stg.setScene(dadosScene);
+				break;
+		}
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
 
 }
-	    
+
