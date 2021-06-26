036ae309c585b58781eae44dbabdcb28f1d603a7
Lucas Henrique Gonzaga de Sales <71048167+hipera09@users.noreply.github.com>
2021-06-26 14:08:02 -0300

    Add files via upload

---
+++ src/ruraldevs/gui/statusController.java
@@ -0,0 +1,82 @@
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import Beans.EstadosEnum;
+import Beans.GrupoEnum;
+import javafx.beans.value.ChangeListener;
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class statusController implements Initializable{
+
+	
+	@FXML
+	Label nomelabel;
+	@FXML
+	Label cpflabel;
+	@FXML
+	Label datalabel;
+	@FXML
+	Label datanasclabel;
+	@FXML
+	Label locallabel;
+	@FXML
+	Label doselabel;
+	@FXML
+	Label vacinalabel;
+	
+	    
+	
+	public void showData() {
+	
+		
+		if(mainTelas.ab.getDose()==0) {mainTelas.ab.setDose(1);}
+		if(mainTelas.ab.getDose()==1) {mainTelas.ab.setDose(2);}
+		
+		System.out.println(mainTelas.ac.getNome());
+		
+		nomelabel.setText("aa");
+		
+		cpflabel.setText(mainTelas.ac.getCpf());
+		
+		datanasclabel.setText(mainTelas.ac.getSenha());
+		
+		vacinalabel.setText(mainTelas.ab.getVacina().getNomeVacina().toString());
+		
+		datalabel.setText(mainTelas.ab.getPessoa().getNome()+"data");
+		
+		locallabel.setText(mainTelas.ab.getLocalVacina().getEnderecoVacina().getCidade());
+		
+		doselabel.setText(Integer.toString(mainTelas.ab.getDose()));
+	}
+	
+
+//	@FXML
+//	 private void initialize() {
+//	
+//	 }
+
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		contentSelect();
+		
+	}
+	private void contentSelect() {
+		showData();
+		
+	}
+
+	 
+	
+}
+
