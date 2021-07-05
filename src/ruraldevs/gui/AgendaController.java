3ed46de6a4b15b6c8718bb4f1471f6107acaa01e
Gian <gdass8000@gmail.com>
2021-07-05 18:15:08 -0300

    Adicionado @FXML as funções de onAction

--- src/ruraldevs/gui/AgendaController.java
+++ src/ruraldevs/gui/AgendaController.java
@@ -1,5 +1,8 @@
 package ruraldevs.gui;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.net.URL;
 import java.time.LocalDate;
 import java.util.Arrays;
@@ -7,6 +10,10 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 import java.util.ResourceBundle;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -15,11 +22,15 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.DateCell;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Hyperlink;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.util.Callback;
+import ruraldevs.beans.CentrosEnum;
+import ruraldevs.beans.DrivesEnum;
 import ruraldevs.beans.Endereco;
 import ruraldevs.beans.EstadosEnum;
 import ruraldevs.beans.GrupoEnum;
@@ -29,179 +40,158 @@ import ruraldevs.beans.Vacina;
 import ruraldevs.beans.VacinasEnum;
 
 public class AgendaController implements Initializable {
+    public static int a = 0;
+    @FXML
+    Hyperlink voltarbttn;
+    @FXML
+    TextField ceptext;
+    @FXML
+    Button bttnagendar;
+    @FXML
+    DatePicker dtVac;
+    @FXML
+    ComboBox<String> centros = new ComboBox<>();
+    @FXML
+    RadioButton driveselect;
+    @FXML
+    ComboBox<String> cidadeselect = new ComboBox<>();
+    @FXML
+    ChoiceBox<GrupoEnum> gruposelect = new ChoiceBox<>();
+    @FXML
+    ComboBox<EstadosEnum> ufselect = new ComboBox<>();
 
-  public static int a = 0;
-  @FXML
-  Hyperlink voltarbttn;
-  @FXML
-  TextField ceptext;
-  @FXML
-  Button bttnagendar;
-  @FXML
-  DatePicker dtVac;
-  @FXML
-  ComboBox<String>centros=new ComboBox< >();
-  @FXML
-  RadioButton driveselect;
-  @FXML
-  ComboBox <String> cidadeselect = new ComboBox < > ();
-  @FXML
-  ChoiceBox < GrupoEnum > gruposelect = new ChoiceBox < > ();
-  @FXML
-  ComboBox < EstadosEnum > ufselect = new ComboBox < > ();
-  @FXML
-  public void voltarpressed(ActionEvent event) {
-	  
-	  cidadeselect.setValue(null);
-	  ceptext.clear();
-	  dtVac.setValue(null);
-	  gruposelect.setValue(null);
-	  ufselect.setValue(null);
-	  
-    mainTelas.changeScreen("main");
-    
-  }
-  
-  public void preencherDrives(ActionEvent event) {
-	  
-	  if(driveselect.isSelected()) {
-	  centros.getItems().clear();  
-	  centros.getItems().addAll(DrivesEnum.names());}
-	  
-	  else {
-		  
-		  centros.getItems().clear();
-		  centros.getItems().addAll(CentrosEnum.names());
-		 }
- 
-	  
-	  
-  }
-  public void preencherCidades(ActionEvent event) {
-	  
-	  EstadosEnum estado = ufselect.getValue();
-	  if (estado.equals("Brasil")) {
-		  return;
-	  }
-	  cidadeselect.getItems().clear();
-	  cidadeselect.setDisable(false);
-	  cidadeselect.getItems().add(null);
-	  JSONParser parser = new JSONParser();
-	  try (FileReader reader = new FileReader(String.format("./src/ruraldevs/data/estados/%s.json", estado.name()))) {
-		  try {
-			  Object obj = parser.parse(reader);
-			  JSONObject estadoJson = (JSONObject) obj;
-			  JSONArray cidadesArray = (JSONArray) estadoJson.get("cidades");
-			  cidadeselect.getItems().addAll(cidadesArray);
-		  } catch (ParseException e) {
-			  System.out.println("PARSE EXCEPTION");
-		  }
-		  
-	  } catch (FileNotFoundException e) {
-		  System.out.println(e);
-	  } catch (IOException e) {
-		  System.out.println("IO EXCEPTION");
-	  }
-	  
-  }
-  
-  
-  public void preencherCentros(ActionEvent event) {
+    @FXML
+    public void voltarpressed(ActionEvent event) {
+        cidadeselect.setValue(null);
+        ceptext.clear();
+        dtVac.setValue(null);
+        gruposelect.setValue(null);
+        ufselect.setValue(null);
 
-		try {
-	 if(cidadeselect.getValue().equals("Recife")) {
-		 centros.getItems().clear();
-		 centros.getItems().addAll(CentrosEnum.names());
-		 centros.setDisable(false);
-		 driveselect.setDisable(false);
-	  }  
-		}catch(Exception e) {}
-  }
+        MainTelas.changeScreen("main");
+    }
 
-  @Override
-  public void initialize(URL location, ResourceBundle resources) {
-	  
-	  
-	 
-	  
-	 
-	  cidadeselect.getItems().add("Escolha um estado");
-	  cidadeselect.setValue("Escolha um estado");
-	  cidadeselect.setDisable(true);
-	  centros.getItems().add("Escolha uma cidade");
-	  centros.setValue("Escolha uma cidade");
-	  centros.setDisable(true);
-	  driveselect.setDisable(true);
-	  
-    Callback < DatePicker, DateCell > dayCellFactory = dp -> new DateCell() {
-      @Override
-      public void updateItem(LocalDate item, boolean empty) {
-        super.updateItem(item, empty);
-
-        if (item.isBefore(LocalDate.now().plusDays(1)) || item.isAfter(LocalDate.now().plusMonths(4))) {
-          setStyle("-fx-background-color: #EBEBEB; -fx-text-fill: darkgray;");
-          setDisable(true);
+    @FXML
+    public void preencherDrives(ActionEvent event) {
+        if (driveselect.isSelected()) {
+            centros.getItems().clear();
+            centros.getItems().addAll(DrivesEnum.names());
         }
-      }
-    };
-    dtVac.setDayCellFactory(dayCellFactory);
 
-    gruposelect.setItems(FXCollections.observableArrayList(GrupoEnum.values()));
-    ufselect.setItems(FXCollections.observableArrayList(EstadosEnum.values()));
-   
-   
-   
-    
-  }
-  
-  
+        else {
+            centros.getItems().clear();
+            centros.getItems().addAll(CentrosEnum.names());
+        }
+    }
 
-  private static final List < VacinasEnum > VALUES = Collections.unmodifiableList(Arrays.asList(VacinasEnum.values()));
-  private static final int SIZE = VALUES.size();
-  private static final Random RANDOM = new Random();
+    public void preencherCidades(ActionEvent event) {
+        EstadosEnum estado = ufselect.getValue();
+        if (estado.equals("Brasil")) {
+            return;
+        }
+        cidadeselect.getItems().clear();
+        cidadeselect.setDisable(false);
+        cidadeselect.getItems().add(null);
+        JSONParser parser = new JSONParser();
+        try (FileReader reader = new FileReader(String.format("./src/ruraldevs/data/estados/%s.json", estado.name()))) {
+            try {
+                Object obj = parser.parse(reader);
+                JSONObject estadoJson = (JSONObject) obj;
+                JSONArray cidadesArray = (JSONArray) estadoJson.get("cidades");
+                cidadeselect.getItems().addAll(cidadesArray);
+            } catch (ParseException e) {
+                System.out.println("PARSE EXCEPTION");
+            }
 
-  public static VacinasEnum random() {
-    return VALUES.get(RANDOM.nextInt(SIZE));
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+        } catch (IOException e) {
+            System.out.println("IO EXCEPTION");
+        }
+    }
 
-  }
+    @FXML
+    public void preencherCentros(ActionEvent event) {
+        try {
+            if (cidadeselect.getValue().equals("Recife")) {
+                centros.getItems().clear();
+                centros.getItems().addAll(CentrosEnum.names());
+                centros.setDisable(false);
+                driveselect.setDisable(false);
+            }
+        } catch (Exception e) {
+        }
+    }
 
-  @FXML
-  public void bttnagendarpressed(ActionEvent event) throws Exception {
-	  Alert alert = new Alert(AlertType.WARNING);
-	  	alert.setTitle("Dados incompletos");
-	  	alert.setHeaderText("Dados vazios ou incorretos");
-	  	alert.setContentText("Por favor, preencha os campos novamente com as informa��es corretas."); 
-	  try {
-	      if (cidadeselect.getValue().equals(null) || ceptext.getText().isEmpty() || dtVac.getValue().equals(null) || gruposelect.getValue().equals(null)
-	    		 || ufselect.getValue().equals(null)) {
-	    	  alert.showAndWait();
-	        return;
-	      }
-	    } catch (NullPointerException e) {
-	    	alert.showAndWait();
-	      return;
-	    }
-	mainTelas.pessoaLogada.setGrupo(gruposelect.getValue()); 
-    LocalVacina LocalVac = new LocalVacina();
-    Endereco EndVac = new Endereco();
-    Vacina Vac = new Vacina();
-    Vac.setNomeVacina(random());
-    EndVac.setCidade(cidadeselect.getValue());
-    EndVac.setCep(ceptext.getText());
-    EndVac.setEstado(ufselect.getValue());
-    LocalVac.setEnderecoVacina(EndVac);
-    LocalVac.setPostoVacinacao(centros.getValue());
-    
-    RegistroVacina cadastro = new RegistroVacina(mainTelas.pessoaLogada, Vac, dtVac.getValue(), LocalVac, gruposelect.getValue());
-    mainTelas.registroVacinaLogado = cadastro;
-    mainTelas.registroController.addRegistroVacina(cadastro);
-    mainTelas.registroVacinaLogado.setDose(0);
-    mainTelas.registroController.salvar();
-    mainTelas.pessoaController.salvar();
-    
-    mainTelas.changeScreen("status");
-    System.out.println(cadastro);
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        cidadeselect.getItems().add("Escolha um estado");
+        cidadeselect.setValue("Escolha um estado");
+        cidadeselect.setDisable(true);
+        centros.getItems().add("Escolha uma cidade");
+        centros.setValue("Escolha uma cidade");
+        centros.setDisable(true);
+        driveselect.setDisable(true);
 
-  }
+        Callback<DatePicker, DateCell> dayCellFactory = dp -> new DateCell() {
+            @Override
+            public void updateItem(LocalDate item, boolean empty) {
+                super.updateItem(item, empty);
 
+                if (item.isBefore(LocalDate.now().plusDays(1)) || item.isAfter(LocalDate.now().plusMonths(4))) {
+                    setStyle("-fx-background-color: #EBEBEB; -fx-text-fill: darkgray;");
+                    setDisable(true);
+                }
+            }
+        };
+        dtVac.setDayCellFactory(dayCellFactory);
+
+        gruposelect.setItems(FXCollections.observableArrayList(GrupoEnum.values()));
+        ufselect.setItems(FXCollections.observableArrayList(EstadosEnum.values()));
+    }
+
+    private static final List<VacinasEnum> VALUES = Collections.unmodifiableList(Arrays.asList(VacinasEnum.values()));
+    private static final int SIZE = VALUES.size();
+    private static final Random RANDOM = new Random();
+
+    public static VacinasEnum random() {
+        return VALUES.get(RANDOM.nextInt(SIZE));
+    }
+
+    @FXML
+    public void bttnagendarpressed(ActionEvent event) throws Exception {
+        Alert alert = new Alert(AlertType.WARNING);
+        alert.setTitle("Dados incompletos");
+        alert.setHeaderText("Dados vazios ou incorretos");
+        alert.setContentText("Por favor, preencha os campos novamente com as informações corretas.");
+        try {
+            if (cidadeselect.getValue().equals(null) || ceptext.getText().isEmpty() || dtVac.getValue().equals(null) || gruposelect.getValue().equals(null) || ufselect.getValue().equals(null)) {
+                alert.showAndWait();
+                return;
+            }
+        } catch (NullPointerException e) {
+            alert.showAndWait();
+            return;
+        }
+        MainTelas.pessoaLogada.setGrupo(gruposelect.getValue());
+        LocalVacina LocalVac = new LocalVacina();
+        Endereco EndVac = new Endereco();
+        Vacina Vac = new Vacina();
+        Vac.setNomeVacina(random());
+        EndVac.setCidade(cidadeselect.getValue());
+        EndVac.setCep(ceptext.getText());
+        EndVac.setEstado(ufselect.getValue());
+        LocalVac.setEnderecoVacina(EndVac);
+        LocalVac.setPostoVacinacao(centros.getValue());
+
+        RegistroVacina cadastro = new RegistroVacina(MainTelas.pessoaLogada, Vac, dtVac.getValue(), LocalVac, gruposelect.getValue());
+        MainTelas.registroVacinaLogado = cadastro;
+        MainTelas.registroController.addRegistroVacina(cadastro);
+        MainTelas.registroVacinaLogado.setDose(0);
+        MainTelas.registroController.salvarArquivo();
+        MainTelas.pessoaController.salvar();
+
+        MainTelas.changeScreen("status");
+        System.out.println(cadastro);
+    }
 }
